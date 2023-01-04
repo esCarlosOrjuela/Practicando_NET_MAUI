652661769b0bb2c9cb8d452546a2d5c65a1b76cc
Carlos Orjuela <ingkrlosorjuela@evertecinc.com>
2023-01-04 07:31:51 -0500

    nuevos cambios

--- Aprendiendo_Net_MAUI/App.xaml.cs
+++ Aprendiendo_Net_MAUI/App.xaml.cs
@@ -2,18 +2,18 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
         /// new NavigationPage(): Creamos un Control de Navegación, definiendo dentro del (), la pagina de inicio.
-		var navigationPage = new NavigationPage(new MainPage());
-		/// Definimos las caracteristicas que tendra el control de navegación creado
-		navigationPage.BarBackground = Colors.Chocolate; 
-		navigationPage.BarTextColor = Colors.White;
-		/// Asignamos el control de navegación como pagina principal de la App
-		MainPage = navigationPage;
-        MainPage = new Mvvm.Views.PageCollectionsControlsDemo();
+		var navigationPage = new NavigationPage(new Mvvm.Views.PageCollectionsControlsDemo());
+        /// Definimos las caracteristicas que tendra el control de navegación creado
+        navigationPage.BarBackground = Colors.Chocolate;
+        navigationPage.BarTextColor = Colors.White;
+        /// Asignamos el control de navegación como pagina principal de la App
+        MainPage = navigationPage;
+        MainPage = new Mvvm.Views.Practica_ColorMaker.MainPage();
 
     }
 }
---
+++ Aprendiendo_Net_MAUI/Mvvm/Views/Practica_ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace Aprendiendo_Net_MAUI.Mvvm.Views.Practica_ColorMaker;
+
+public partial class MainPage : ContentPage
+{
+    bool isRandom;
+    string hexValue;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void Sliders_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+    {
+        if (!isRandom)
+        {
+            var red = this.SliderRed.Value;
+            var green = this.SliderGreen.Value;
+            var blue = this.SliderBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+        btnRandom.BackgroundColor = color;
+        Container.BackgroundColor = color;
+        hexValue = color.ToHex();
+        lblHex.Text = hexValue;
+        lblHex.TextColor = color;
+
+
+    }
+
+    void btnRandom_Clicked(System.Object sender, System.EventArgs e)
+    {
+        isRandom = true;
+        var random = new Random();
+        var color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        SetColor(color);
+
+        SliderRed.Value = color.Red;
+        SliderBlue.Value = color.Blue;
+        SliderGreen.Value = color.Green;
+        isRandom = false;
+    }
+
+    async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Clipboard.SetTextAsync(hexValue);
+        var toast = Toast.Make("Color Copied", CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+        await toast.Show();
+    }
+}
